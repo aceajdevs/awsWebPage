1b7c2195c2c5273050f8a56431e0123ff3fe9e99
DuckDonni <jonahsmith2010@gmail.com>
2026-01-31 18:11:08 -0500

    modified img src

--- cloudp1/src/pages/Home.jsx
+++ cloudp1/src/pages/Home.jsx
@@ -4,8 +4,8 @@ function Home() {
     const imageUrl = "https://bucketofducks730335186499.s3.us-east-1.amazonaws.com/duckbucket.png";
     return(
         <div>
-            <h1>Welcome to the Home Page</h1>
-            {imageUrl && <img src={imageUrl} alt="Duck in Bucket" />}
+            <h1>Home Page</h1>
+            <img src={imageUrl} alt="Duck in Bucket" />
         </div>
     )
 }
