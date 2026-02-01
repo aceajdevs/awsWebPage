8b740d1b5782b1d7a49087256c15acf341e40070
DuckDonni <jonahsmith2010@gmail.com>
2026-01-31 19:19:05 -0500

    updated with assignment requirements

--- cloudp1/src/pages/About.jsx
+++ cloudp1/src/pages/About.jsx
@@ -1,7 +1,12 @@
 function About() {
     return(
-        <div>
-            <h1>Welcome to the About Page</h1> 
+        <div style={{ width: "30%", margin: "0 auto" }}>
+            <h1 align="center">Created for my SWE 4633 class</h1>
+            <p align="center">
+                Hello, my name is Jonah Smith. I am a senior software engineer here at Kennesaw State University. 
+                In my free time I like working on my passion projects and working out. I also enjoy hiking and playing volleyball as well.
+                I am excited to learn more about cloud computing and AWS through this course.
+            </p>
         </div>
     )
 }
